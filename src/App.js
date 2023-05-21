9e607fae53532b460f1f8d65a27ea84352b0e1e8
Hamas Shafiq <hshafiq@hotmail.co.uk>
2023-05-21 19:50:36 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Dropdown, Container } from "semantic-ui-react";
+import Papa from "papaparse";
+import Plot from "react-plotly.js";
 
-function App() {
+let subs = [];
+const dropdownOptions = [];
+const frequencies = ["10", "12.5", "16", "20", "25", "31.5", "40", "50", "63", "80", "100", "125"];
+
+const MovieData = () => {
+  const [selectedSubs, setSelectedSubs] = useState([]);
+  const [plotData, setPlotData] = useState([]);
+  const [data, setData] = useState({});
+  const [timesDataSet, setTimesDataSet] = useState(0);
+
+  useEffect(() => {
+    Papa.parse(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vS9JcTRFq0ha9P-abs1Yh1eF3I236kKWS-cD2h3x2K3M_ytkFY5qUHZYHnkkoynVGrS4ShPBry80rx0/pub?gid=732039419&single=true&output=csv",
+      {
+        download: true,
+        complete: (results) => {
+          const result = {};
+
+          for (let i = 0; i < results.data.length; i++) {
+            const [key, ...values] = results.data[i];
+            result[key] = values;
+          }
+
+          setData(result);
+          setTimesDataSet((val) => val + 1);
+          const keys = Object.keys(result);
+          keys.shift();
+          subs = keys;
+        },
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (timesDataSet === 2) {
+      subs.forEach((sub, i) => {
+        dropdownOptions.push({ key: sub, value: sub, text: sub });
+      });
+    }
+  }, [timesDataSet]);
+
+  useEffect(() => {
+    if (!selectedSubs.length) setPlotData([]);
+
+    const traces = [];
+
+    selectedSubs.forEach((sub) => {
+      const trace = {
+        x: frequencies,
+        y: data[sub].slice(1, -1),
+        name: sub,
+      };
+      traces.push(trace);
+      setPlotData(traces);
+    });
+  }, [selectedSubs]);
+
+  const handleSubsSelected = (_, { value }) => {
+    setSelectedSubs(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container textAlign="center">
+      {timesDataSet === 2 ? (
+        <div>
+          <Dropdown
+            placeholder="Select Movie"
+            onChange={handleSubsSelected}
+            fluid
+            multiple
+            search
+            selection
+            options={dropdownOptions}
+          />
+          <Plot
+            data={plotData}
+            layout={{
+              autosize: true,
+              xaxis: {
+                title: "Frequency (Hz)",
+                tickMode: "linear",
+              },
+              yaxis: {
+                title: "dB",
+              },
+            }}
+            style={{ width: "100%", height: "100%" }}
+            useResizeHandler={true}
+          />
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </Container>
   );
-}
+};
 
-export default App;
+export default MovieData;
